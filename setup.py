4a4deacfe9c03ef7c2591dbbe8ca4b798a1c5164
Alan Jeanpierre <ajeanpierre825@gmail.com>
2018-03-13 20:50:16 -0700

    Fix agnes source typo

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from distutils.core import setup, Extension
 import numpy
 
 kmeans = Extension('_kmeans', sources=['kmeans_wrap.cxx', 'kmeans.cc'], include_dirs=[numpy.get_include()])
-agnes = Extension('_agnes', sources=['agnes_wrap.cxx', 'kmeans.cc'], include_dirs=[numpy.get_include()])
+agnes = Extension('_agnes', sources=['agnes_wrap.cxx', 'agnes.cc'], include_dirs=[numpy.get_include()])
 dbscan = Extension('_dbscan', sources=['dbscan_wrap.cxx', 'dbscan.cc'], include_dirs=[numpy.get_include()])
 
 setup( name = 'diycluster',
