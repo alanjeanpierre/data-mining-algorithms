fb5502d9b4f878f9692207963c1b6fcefc0b0876
alanjeanpierre <ajeanpierre825@gmail.com>
2018-03-15 18:20:24 -0700

    Change setup file to explicitly use C++11 std

--- setup.py
+++ setup.py
@@ -11,13 +11,19 @@ from distutils.core import setup, Extension
 import numpy
 kmeans = Extension('_kmeans',
                     sources=['kmeans_wrap.cxx', 'kmeans.cc'], 
-                    include_dirs=[numpy.get_include()],)
+                    include_dirs=[numpy.get_include()],
+                    extra_compile_args=['-std=c++11'],
+                    )
 agnes = Extension('_agnes',
                   sources=['agnes_wrap.cxx', 'agnes.cc'], 
-                  include_dirs=[numpy.get_include()], )
+                  include_dirs=[numpy.get_include()], 
+                  extra_compile_args=['-std=c++11'],
+                  )
 dbscan = Extension('_dbscan', 
                    sources=['dbscan_wrap.cxx', 'dbscan.cc'], 
-                   include_dirs=[numpy.get_include()], )
+                   include_dirs=[numpy.get_include()], 
+                   extra_compile_args=['-std=c++11'],
+                   )
 
 setup( name = 'diycluster',
        version = '0.1',
