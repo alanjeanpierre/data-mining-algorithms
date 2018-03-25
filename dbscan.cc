6e6fa7a06b93846a82252adab2ac690fe701ed17
blackmnky <lre12a@acu.edu>
2018-03-25 18:01:58 -0500

    working on dbscan::fit

--- dbscan.cc
+++ dbscan.cc
@@ -5,6 +5,7 @@
 #include <cmath>
 #include <map>
 #include <algorithm>
+ #include <cstdlib>
 
 /*
 Core Cluster -- if the cluster has more than minPoints in or touching it
@@ -19,7 +20,7 @@ DBSCAN::DBSCAN(int eps, int minPoints){
 	if(eps < 1 || minPoints < 1){
 		eps = 1;
 		minPoints = 1;
-		std::cerr << "Error: values must be positive non-zero" << std::end1;
+		std::cerr << "Error: values must be positive non-zero" << std::endl;
 	}
 
 	// save the values
@@ -50,6 +51,23 @@ void DBSCAN::Fit(double * arr, int rows, int cols){
 
 	// copy data to internal data structure
 	CopyData(arr, rows, cols);
+
+	// choose a random point
+	double rand = 0.0;
+	
+	// loop through until every point is classified
+	while(classified->size() != data->size()){ //not every point is classified
+		if(isClassified(rand))
+			continue;
+		else{
+			neighborhood = Neighborhood(rand);
+			if(neighborhood->size() < minPts) //not core
+				noise->push_back(rand);
+			else{ //core point
+				//start cluster around core point
+			}
+		}
+	}
 	
 }
 
@@ -57,39 +75,56 @@ void DBSCAN::Fit(double * arr, int rows, int cols){
 Point 'p' is density reachable from point 'q' if point 'p' is within eps distance from 'q' and 
 'q' has minPoints in its neighbors within distance eps
 */
-std::vector<int> * DBSCAN::DensityReach(int index){
+std::vector<int> * DBSCAN::DensityReach(int p, int q){
 
 }
 
 /*
-Find the neighboorhood of a point and return it in a vector
+Find the neighboorhood of a point and return it in a vector; Same thing as DensityReach
 */
-std::vector<int> * DBSCAN::Neighborhood(int index){
-	std::std::vector<int> neighbors;
-	
+std::vector<double> * DBSCAN::Neighborhood(int index){
+	std::vector<double> neighbors;
+
+
 }
 
 /*
-Check to see if a certain point is inside of a core cluster
+Check to see if a certain point is a core point
 */
 bool DBSCAN::isCoreObject(double point){
-	return std::find(core.begin(), core.end(), point) != core.end();
+	if(std::find(core->begin(), core->end(), point) != core->end()){
+		classified->push_back(point);
+		return true;
+	}else
+		return false;
 }
 
 /*
 Check to see if a certain point is inside of a border cluster
 */
 bool DBSCAN::isBorderObject(double point){
-	return std::find(border.begin(), border.end(), point) != border.end();
+	if(std::find(border->begin(), border->end(), point) != border->end()){
+		classified->push_back(point);
+		return true;
+	}else
+		return false;
+	
 }
 
 /*
 Check to see if a certain point is inside of a outlier cluster
 */
-bool isOutlierObject(double point){
-	return std::find(outlier.begin(), outlier.end(), point) != outlier.end();
+bool DBSCAN::isOutlierObject(double point){
+	if(std::find(noise->begin(), noise->end(), point) != noise->end()){
+		classified->push_back(point);
+		return true;
+	}else
+		return false;
 }
 
+bool DBSCAN::isClassified(double point){
+	return std::find(classified->begin(), classified->end(), point) != classified->end();
+}
 
 void DBSCAN::GetLabels(int * out, int n){
 
