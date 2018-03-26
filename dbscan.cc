cf06e1b6fc3e4eea34fee4273fd2a19f36bf2823
blackmnky <lre12a@acu.edu>
2018-03-25 19:03:36 -0500

    changed core and bored to type map

--- dbscan.cc
+++ dbscan.cc
@@ -54,7 +54,7 @@ void DBSCAN::Fit(double * arr, int rows, int cols){
 
 	// choose a random point
 	double rand = 0.0;
-	
+
 	// loop through until every point is classified
 	while(classified->size() != data->size()){ //not every point is classified
 		if(isClassified(rand))
@@ -64,7 +64,7 @@ void DBSCAN::Fit(double * arr, int rows, int cols){
 			if(neighborhood->size() < minPts) //not core
 				noise->push_back(rand);
 			else{ //core point
-				//start cluster around core point
+				//core->insert( std::make_pair(rand, neighborhood) );	
 			}
 		}
 	}
@@ -72,56 +72,31 @@ void DBSCAN::Fit(double * arr, int rows, int cols){
 }
 
 /*
-Point 'p' is density reachable from point 'q' if point 'p' is within eps distance from 'q' and 
-'q' has minPoints in its neighbors within distance eps
-*/
-std::vector<int> * DBSCAN::DensityReach(int p, int q){
-
-}
-
-/*
 Find the neighboorhood of a point and return it in a vector; Same thing as DensityReach
 */
-std::vector<double> * DBSCAN::Neighborhood(int index){
+std::vector<double> * DBSCAN::Neighborhood(double index){
 	std::vector<double> neighbors;
 
 
 }
 
 /*
-Check to see if a certain point is a core point
+Find the distance between two points
 */
-bool DBSCAN::isCoreObject(double point){
-	if(std::find(core->begin(), core->end(), point) != core->end()){
-		classified->push_back(point);
-		return true;
-	}else
-		return false;
-}
+double DBSCAN::Distance(int p, int q){
 
-/*
-Check to see if a certain point is inside of a border cluster
-*/
-bool DBSCAN::isBorderObject(double point){
-	if(std::find(border->begin(), border->end(), point) != border->end()){
-		classified->push_back(point);
-		return true;
-	}else
-		return false;
-	
 }
 
 /*
 Check to see if a certain point is inside of a outlier cluster
 */
 bool DBSCAN::isOutlierObject(double point){
-	if(std::find(noise->begin(), noise->end(), point) != noise->end()){
-		classified->push_back(point);
-		return true;
-	}else
-		return false;
+	return std::find(noise->begin(), noise->end(), point) != noise->end();
 }
-
+ 
+/*
+Returns true is a point has already be classified, otherwise turns false
+*/
 bool DBSCAN::isClassified(double point){
 	return std::find(classified->begin(), classified->end(), point) != classified->end();
 }
--- dbscan.h
+++ dbscan.h
@@ -7,20 +7,20 @@
 class DBSCAN {
 
 private:
-	bool isCoreObject(double point);
-	bool isBorderObject(double point);
+	bool isCoreObject(double point); //may not be needed
+	bool isBorderObject(double point); //may not be needed
 	bool isOutlierObject(double point);
 	bool isClassified(double point);
 	void CopyData(double *arr, int rows, int cols);
-	std::vector<double> * Neighborhood(int index);
-	std::vector<int> * DensityReach(int p, int q);
+	double Distance(int p, int q);
+	std::vector<double> * Neighborhood(double index);
 	std::vector<std::vector<double> > * data;
 	std::vector<std::vector<double> > * distmatrix;
 	std::vector<double> * classified;
 	std::vector<double> * neighborhood;
-	std::vector<double> * noise;
-	std::vector<double> * core;
-	std::vector<double> * border;
+	std::vector<double> * noise; //possible change to multimap
+	std::map<double, std::vector<double> > * core; //possible change to multimap
+	std::map<double, std::vector<double> > * border; //possible change to multimap
 	int radius;
 	int minPts;
 	int n_clusters;
