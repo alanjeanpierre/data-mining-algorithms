4c50abd70122e061da7f6862da19e159be966731
Alan Jeanpierre <ajeanpierre825@gmail.com>
2018-04-06 16:30:38 -0700

    Change benchmarks file

--- benchmarks.py
+++ benchmarks.py
@@ -5,42 +5,43 @@ import numpy as np
 import agnes
 import kmeans
 import dbscan
+import platform, os, subprocess
+
+opsys = platform.system()
+osver = platform.release()
+gccver = subprocess.check_output(['g++', '--version']).decode('utf-8').split('\n')[0]
+pyver = platform.python_version()
 
 n = 20
 if '-n' in sys.argv:
     n = int(sys.argv[sys.argv.index('-n')+1])
 
-increment = 100
-if '-i' in sys.argv:
-    increment = int(sys.argv[sys.argv.index('-i')+1])
-
-max_iter = 2000
+r = list()
 if '-r' in sys.argv:
-    max_iter = int(sys.argv[sys.argv.index('-r')+1])
+    for i in sys.argv[sys.argv.index('-r')+1:]:
+        r.append(int(i))
 
-ref = False
-if '-p' in sys.argv:
-    ref = True
-
+print('Running on {} v. {}'.format(opsys, osver))
+print('Algorithms were compiled with {}'.format(gccver))
+print('Running on Python version {}'.format(pyver))
 print('Benchmarking KMeans, Agnes, and DBSCAN implementations \nagainst their sklearn reference implementations')
-print('Each time is an average of {:d} rounds against the same dataset'.format(n))
-if ref:
-    print('Times are represented as a ratio of ref/impl')
+print('Each time is an average of {:d} rounds against the same dataset in seconds'.format(n))
 print()
-print('{:5s}\t{:10s}\t{:10s}\t{:10s}'.format('Npts', 'Kmeans', 'Agnes', 'DBSCAN'))
-for num in range(100, max_iter, increment):
+print('{:5s}\t{:10s}\t{:10s}\t{:10s}\t{:10s}\t{:10s}\t{:10s}'.format('Npts', 'KmeansSK', 'KmeansC++', 'AgnesSK', 'AgnesC++', 'DBSCANSK', 'DBSCANC++'))
+
+for num in r:
     blobs, _ = datasets.make_blobs(num)
 
 
-    if ref:
-        t_total = 0.0
-        for i in range(n):
-            c = cluster.KMeans(3)
-            t0 = time.time()
-            c.fit(blobs)
-            t1 = time.time()
-            t_total += (t1-t0)
-        kref = t_total/n
+    
+    t_total = 0.0
+    for i in range(n):
+        c = cluster.KMeans(3)
+        t0 = time.time()
+        c.fit(blobs)
+        t1 = time.time()
+        t_total += (t1-t0)
+    kref = t_total/n
     
     t_total = 0.0
     for i in range(n):
@@ -51,15 +52,15 @@ for num in range(100, max_iter, increment):
         t_total += (t1-t0)
     kimpl = t_total/n
     
-    if ref:
-        t_total = 0.0
-        for i in range(n):
-            c = cluster.AgglomerativeClustering(3)
-            t0 = time.time()
-            c.fit(blobs)
-            t1 = time.time()
-            t_total += (t1-t0)
-        aref = t_total/n
+
+    t_total = 0.0
+    for i in range(n):
+        c = cluster.AgglomerativeClustering(3)
+        t0 = time.time()
+        c.fit(blobs)
+        t1 = time.time()
+        t_total += (t1-t0)
+    aref = t_total/n
     
     t_total = 0.0
     for i in range(n):
@@ -70,16 +71,16 @@ for num in range(100, max_iter, increment):
         t_total += (t1-t0)
     aimpl = t_total/n
     
-    if ref:
-        t_total = 0.0
-        for i in range(n):
-            c = cluster.DBSCAN(1.5, 5)
-            t0 = time.time()
-            c.fit(blobs)
-            t1 = time.time()
-            t_total += (t1-t0)
-        dref = t_total/n
-    
+
+    t_total = 0.0
+    for i in range(n):
+        c = cluster.DBSCAN(1.5, 5)
+        t0 = time.time()
+        c.fit(blobs)
+        t1 = time.time()
+        t_total += (t1-t0)
+    dref = t_total/n
+
     
     t_total = 0.0
     for i in range(n):
@@ -89,9 +90,6 @@ for num in range(100, max_iter, increment):
         t1 = time.time()
         t_total += (t1-t0)
     dimpl = t_total/n
-        
-    if ref:
-        print('{:<5d}\t{:<10.4f}\t{:<10.4f}\t{:<10.4f}'.format(num, kref/kimpl, aref/aimpl, dref/dimpl))
-    else:
-        print('{:<5d}\t{:<10.4f}\t{:<10.4f}\t{:<10.4f}'.format(num, kimpl, aimpl, dimpl))
+
+    print('{:<5d}\t{:<10.4f}\t{:<10.4f}\t{:<10.4f}\t{:<10.4f}\t{:<10.4f}\t{:<10.4f}'.format(num, kref, kimpl, aref, aimpl, dref, dimpl))
 
